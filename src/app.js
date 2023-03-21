7d92335eff5a0cfedc89ee65901eb0981ef58249
sveta-1970 <ssarisoydan@gmail.com>
2023-03-21 18:26:44 +0200

    Added weather engine for city

---
+++ src/app.js
@@ -0,0 +1,35 @@
+//search engine, when searching for a city,
+//display the city name on the page after the user submits the form
+//and the current temperature of the city.
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector(".weather").innerHTML = response.data.weather[0].main;
+  document.querySelector(".humidity").innerHTML = response.data.main.humidity;
+  document.querySelector(".wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "52b789a53d83895c9bd9e318a67b4fa8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  if (city) {
+    searchCity(city);
+  } else {
+    alert("Please enter a city");
+  }
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+searchCity("Kyiv");
