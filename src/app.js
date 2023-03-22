801014dc04d746faab8178fc91aebc4dd8724608
sveta-1970 <ssarisoydan@gmail.com>
2023-03-23 00:11:10 +0200

    Changed margins, added units conversion

--- src/app.js
+++ src/app.js
@@ -48,6 +48,8 @@ function displayWeather(response) {
   document
     .querySelector(".icon")
     .setAttribute("alt", response.data.weather[0].description);
+
+  celsiusTemperature = Math.round(response.data.main.temp);
 }
 
 function searchCity(city) {
@@ -67,6 +69,36 @@ function handleSubmit(event) {
   }
 }
 
+//Celsius to Farenheit
+
+function convertCelsiusToFarenheit(event) {
+  event.preventDefault();
+  //remove the active class from celsius-link
+  celsiusLink.classList.remove("active");
+  //add the  active class to farenheit-link
+  farenheitLink.classList.add("active");
+  let farenheitTemperature = Math.round(celsiusTemperature * 1.8 + 32);
+  document.querySelector("#main-temperature").innerHTML = farenheitTemperature;
+}
+
+//Farenheit to Celsius
+
+function convertFarenheitToCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  farenheitLink.classList.remove("active");
+  document.querySelector("#main-temperature").innerHTML = celsiusTemperature;
+  return;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 searchCity("Kyiv");
+
+let celsiusTemperature = null;
+
+let farenheitLink = document.querySelector("#farenheit-link");
+farenheitLink.addEventListener("click", convertCelsiusToFarenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertFarenheitToCelsius);
