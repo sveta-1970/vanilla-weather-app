fe9081273f81b19963011ecec2eb6bc8f7b63792
sveta-1970 <ssarisoydan@gmail.com>
2023-03-24 22:11:02 +0200

    Removed conversion to farenheit

--- src/app.js
+++ src/app.js
@@ -28,15 +28,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
@@ -47,21 +39,26 @@ function displayForecast(response) {
   let forecastHTML = "";
   forecast.forEach(function (forecastDay, index) {
     if (index > 0 && index < 7) {
-
-    forecastHTML =
-      forecastHTML +
-      `
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col">
               <p class="weather-forecast-date">${formatDay(forecastDay.dt)}</p>
               <p class="weather-forecast-icon">
-                <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png" alt="">             
+                <img src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }.png" alt="">             
               </p>
               <p class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
-                <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
             </div>
             `;
-     }       
+    }
   });
 
   forecastElement.innerHTML = forecastHTML;
@@ -70,7 +67,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "c8a77112b2faf6684bb4b21a0aa778ae";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -101,8 +98,6 @@ function displayWeather(response) {
     .setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
-
-  celsiusTemperature = Math.round(response.data.main.temp);
 }
 
 function searchCity(city) {
@@ -122,36 +117,6 @@ function handleSubmit(event) {
   }
 }
 
-//Celsius to Farenheit
-
-function convertCelsiusToFarenheit(event) {
-  event.preventDefault();
-  //remove the active class from celsius-link
-  celsiusLink.classList.remove("active");
-  //add the  active class to farenheit-link
-  farenheitLink.classList.add("active");
-  let farenheitTemperature = Math.round(celsiusTemperature * 1.8 + 32);
-  document.querySelector("#main-temperature").innerHTML = farenheitTemperature;
-}
-
-//Farenheit to Celsius
-
-function convertFarenheitToCelsius(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  farenheitLink.classList.remove("active");
-  document.querySelector("#main-temperature").innerHTML = celsiusTemperature;
-  return;
-}
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 searchCity("Kyiv");
-
-let celsiusTemperature = null;
-
-let farenheitLink = document.querySelector("#farenheit-link");
-farenheitLink.addEventListener("click", convertCelsiusToFarenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertFarenheitToCelsius);
